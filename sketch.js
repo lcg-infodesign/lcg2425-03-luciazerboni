db7d161b53899310c3ac2ab2803e53d63ca6dc30
luciazerboni <lucia.zerboni@mail.polimi.it>
2024-11-25 23:57:04 +0100

    def

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let table;
+let ralewayFont;
 
 function preload() {
   // Carica il file CSV
@@ -7,16 +8,22 @@ function preload() {
     'csv',
     'header'
   );
+
+  // Carica font
+  //ralewayFont = loadFont('https://fonts.gstatic.com/s/raleway/v28/1Ptsg8zYS_SKggPN4iEgvnHyvveLxVvaoQI.woff2');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, 5150);
   background('white');
   noLoop();
 
+  // Imposta font
+  //textFont(ralewayFont);
+
   let xStart = 50; // Punto iniziale delle linee sull'asse x
-  let y = 100; // Punto iniziale sull'asse y (spazio per titolo e sottotitolo)
-  let lineHeight = 30; // Spaziatura tra linee
+  let y = 160; // Punto iniziale sull'asse y (spazio per titolo e sottotitolo)
+  let lineHeight = 50; // Spaziatura tra linee
   let maxLength = 700; // Lunghezza max linee
 
   // Colori temperatura
@@ -26,10 +33,12 @@ function setup() {
   // titolo
   fill(0);
   textAlign(CENTER, CENTER);
+  textStyle(BOLD);
   textSize(24);
-  text("Rivers in the World", width / 2, 30); // Titolo principale
+  text("Rivers in the World", width / 2, 50); // Titolo principale
   textSize(16);
-  text("Graphical representation of the world's major rivers", width / 2, 60); // Sottotitolo
+  textStyle(NORMAL);
+  text("Graphical representation of the world's major rivers", width / 2, 80); // Sottotitolo
 
   // Trova valori massimi e minimi per lunghezza, portata e temperatura
   let maxRiverLength = 0;
@@ -62,6 +71,7 @@ function setup() {
     let riverLength = table.getNum(r, 'length'); // Lunghezza del fiume
     let riverDischarge = table.getNum(r, 'discharge'); // Portata del fiume
     let riverTemp = table.getNum(r, 'avg_temp'); // Temperatura media del fiume
+    let riverCountry = table.getString(r, 'countries');
 
     // Scala la lunghezza del fiume rispetto alla lunghezza massima
     let scaledLength = map(riverLength, 0, maxRiverLength, 0, maxLength);
@@ -79,15 +89,17 @@ function setup() {
 
     // nome fiume
     noStroke();
-    textStyle(BOLD)
+    textStyle(BOLD);
     fill(0);
     textAlign(LEFT, CENTER);
+    textSize(15)
     text(riverName, xStart + scaledLength + 10, y);
 
-    // temperatura media
-    textStyle(NORMAL)
-    textSize(12);
-    text(`${riverTemp}°C`, xStart + scaledLength + 10, y + 10);
+    // temperatura media e paese
+    textStyle(NORMAL);
+    textSize(13);
+    text(riverCountry, xStart + scaledLength + 10, y + 15);
+    text(`${riverTemp}°C`, xStart + scaledLength + 10, y + 30);
 
     // Sposta il punto y per la prossima linea
     y += lineHeight;
